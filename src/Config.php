93f845b61e6941cf7852fd64bdc1ead8a66834f9
nece001 <nece001@163.com>
2023-06-18 20:10:05 +0800

    '修改说明'

--- src/Config.php
+++ src/Config.php
@@ -16,8 +16,8 @@ class Config extends ConfigAbstract
         $this->addTemplate(false, 'sub_path', '子目录', '例：a/b');
 
         $this->addTemplate(false, 'schema', '协议头部', '默认为https');
-        $this->addTemplate(false, 'timeout', '请求超时', '秒', '600');
-        $this->addTemplate(false, 'connect_timeout', '连接超时', '秒', '60');
+        $this->addTemplate(false, 'timeout', 'Socket层传输数据的超时时间', '秒', '600');
+        $this->addTemplate(false, 'connect_timeout', '建立连接的超时时间', '秒', '60');
         $this->addTemplate(false, 'proxy', '代理', '例：http://xxx:xx');
     }
 }
